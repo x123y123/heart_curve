78f9c78b5c3c1a623669ff1d975c9c2465e5ae03
tony <a0921444212@gmail.com>
2023-01-15 17:02:12 +0800

    update shape range

--- heart_curve_01.c
+++ heart_curve_01.c
@@ -5,16 +5,16 @@
 The heart shape is use formula:
 (x^2 + y^2 -1)^3 = (x^2)*(y^3)
 
-x ~ (-1.2, 1.2)
-y ~ (-1.1, 1.3)
+x ~ (-1.5, 1.5)
+y ~ (-1, 2)
 -----------------------------------*/
 int main()
 {
     float xstep_size = 0.025f;
     float ystep_size = 0.05f;
 
-    for (float y = 1.3; y >= -1.1; y -= ystep_size) {
-        for (float x = -1.2; x <= 1.2; x += xstep_size) {
+    for (float y = 2.0f; y >= -1.0f; y -= ystep_size) {
+        for (float x = -1.5f; x <= 1.5f; x += xstep_size) {
             if (pow((pow(x, 2) + pow(y, 2) - 1.0), 3) - pow(x, 2) * pow(y, 3) <= 0.0)
                 printf("@");
             else
